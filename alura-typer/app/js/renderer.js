4a68cacf7ef52f4508bff4345ce7cd751d9024c7
Marcelo Valderrama Correa <marcelo.correa@mjv.com.br>
2020-06-13 22:02:26 -0300

    Aula Hoje

--- alura-typer/app/js/renderer.js
+++ alura-typer/app/js/renderer.js
@@ -1,9 +1,29 @@
 const { ipcRenderer } = require('electron');
+const timer = require('./timer');
 
 let link = document.querySelector('#link');
+let botaoPlay = document.querySelector('.botao-play');
+let tempo = document.querySelector('.tempo');
 
 console.log('link'+ link);
 link.addEventListener('click', function() {
     ipcRenderer.send('abrir-janela-sobre');
 
 });
+
+
+let imagens = ['img/play-button.svg', 'img/stop-button.svg'];
+let play = false;
+botaoPlay.addEventListener('click', () => {
+
+    if (play){
+        timer.parar();
+        play = false;
+
+    }else{
+        timer.iniciar(tempo);
+        play = true;
+    }
+     imagens = imagens.reverse()
+    botaoPlay.src = imagens[0];
+})
