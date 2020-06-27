d62bd501291973fcb71a54af2127c577d6b65ec7
Marcelo Valderrama Correa <marcelo.correa@mjv.com.br>
2020-06-27 16:35:51 -0300

    Commit Final
    Commit Final

--- alura-typer/main.js
+++ alura-typer/main.js
@@ -1,14 +1,29 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain , Tray, Menu, globalShortcut} = require('electron');
+const data = require('./data');
+const templateGenerator = require('./template');
 
+
+let tray = null;
+let mainWindow = null;
 app.on('ready', () => {
     console.log('PORRRAAAAAAAAA');
-    let mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         width: 600,
         height: 400,
         webPreferences: {
             nodeIntegration: true
         }
     });
+
+    tray = new Tray(__dirname+'/app/img/icon-tray.png');
+    let template = templateGenerator.geraTrayTemplate(mainWindow);
+    let templateMenu = templateGenerator.geraMenuPrincipalTemplate(app);
+    let trayMenu = Menu.buildFromTemplate(templateMenu);
+    Menu.setApplicationMenu(trayMenu)
+    globalShortcut.register('CommandOrControl+Shift+S', () =>{
+       mainWindow.send('atalho-iniciar-parar');
+    })
+
 
     console.log(`${__dirname}`);
     mainWindow.loadURL(`file://${__dirname}/app/index.html`);
@@ -46,3 +61,14 @@ ipcMain.on('abrir-janela-sobre', () => {
 ipcMain.on('fechar-janela-sobre', () =>{
     sobreWindows.close();
 })
+
+ipcMain.on('curso-parado', (event, curso, tempoEstudado) => {
+console.log(`O curso ${curso} foi estudado em um tempo de ${tempoEstudado}`);
+data.salvaDados(curso, tempoEstudado);
+});
+
+ipcMain.on('curso-adicionado', (event, novoCurso)=> {
+    let novoTemplate = templateGenerator.adicionaCursoNoTray(novoCurso, mainWindow);
+    let novoTrayMenu = Menu.buildFromTemplate(novoTemplate);
+    tray.setContextMenu(novoTrayMenu);
+});
---
+++ alura-typer/template.js
@@ -0,0 +1,96 @@
+const data = require('./data');
+const { ipcMain } = require('electron');
+
+module.exports = {
+    templateInicial: null,
+
+
+    geraTrayTemplate(window) {
+        let template = [
+            {
+                'label': 'Cursos'
+            },
+            {
+                type: 'separator'
+            }
+        ];
+
+        let cursos = data.pegaNomeDosCursos();
+        cursos.forEach((curso) => {
+            let menuItem = {
+                label: curso,
+                type: 'radio',
+                click: () => {
+                    window.send('curso-trocado', curso);
+                }
+            }
+            template.push(menuItem);
+        })
+        this.templateInicial = template;
+        return template;
+    },
+
+    adicionaCursoNoTray(curso, window) {
+        this.templateInicial.push({
+            label: curso,
+            type: 'radio',
+            checked: true,
+            click: () => {
+                window.send('curso-trocado', curso);
+            }
+        })
+
+        return this.templateInicial;
+    },
+    geraMenuPrincipalTemplate(app){
+        let templateMenu = [
+            {
+                label: 'View',
+                submenu: [{
+                    role: 'reload'
+                },
+                {
+                    role: 'toggledevtools'
+                  }
+                ]
+            },
+            {
+                label: 'Window',
+                submenu: [
+                    {
+                        role: 'minimize', 
+                        accelerator: 'Alt+M'                     
+                    },
+                    {
+                        role: 'close'
+                    }
+                ]
+            },
+            {
+                label: 'Sobre',
+                submenu: [
+                    {
+                        label: 'Sobre o Alura Timer',
+                        click: () => {
+                            ipcMain.emit('abrir-janela-sobre');
+                        },
+                        accelerator: 'CommandOrControl+I'
+                    }
+                ]
+            }
+        ];
+        if( process.platform == 'darwin'){
+            templateMenu.unshift({
+                label: app.getName(),
+                submenu: [
+                    {
+                        label: 'Estou rodando no Mac!'
+                    }
+                ]
+            })
+         }
+         console.log(templateMenu);
+         return templateMenu;
+    }
+
+}
